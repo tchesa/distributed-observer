682fb373dc338568ac590872e529bb49eec0bd79
Cesar <cesarolimpio@gmail.com>
2018-12-03 22:40:56 -0200

    redundancy servers

--- server.py
+++ server.py
@@ -50,20 +50,33 @@ import threading
 from message import Message
 import pickle
 import sys
+import uuid
 
 class Server:
   host = '127.0.0.1'
   port = 5000
   observers = []
-  master = False
+  servers = []
+  isMaster = False
   sock = socket.socket()
+  # master = []
+  id = -1
 
-  def __init__(self, host, port, isMaster=False):
-    self.host = host
+  def __init__(self, port, masterHost, masterPort):
     self.port = port
-    self.master = isMaster
+    self.id = str(uuid.uuid1()) # based on ip and time
+    if masterHost and masterPort:
+      self.servers.append((masterHost, masterPort, ''))
+      self.isMaster = False
+    else:
+      self.servers.append((self.host, self.port, self.id))
+      self.isMaster = True
 
-  def handler (self, c, a):
+  def notifyAll(self, message):
+    for connection in self.observers:
+      connection.send(message.encode())
+
+  def handler(self, c, a):
     while True:
       data = c.recv(1024)
       if not data:
@@ -71,14 +84,41 @@ class Server:
         self.observers.remove(c)
         c.close()
         break
-      for connection in self.observers:
-        message = Message('message', data.decode('utf8'))
-        encoded = pickle.dumps(message)
-        connection.send(encoded)
+      received = pickle.loads(data)
+      print(received, '({} bytes)'.format(len(data)))
+      if received.header == 'registerServer':
+        self.servers.append(received.body)
+        self.notifyAll(Message('updateServers', self.servers))
+      # for connection in self.observers:
+        # message = Message('message', data.decode('utf8'))
+        # encoded = pickle.dumps(message)
+        # connection.send(encoded)
 
   def run(self):
+    if (not self.isMaster): # listen to MASTER updates
+      print(self.servers)
+      while len(self.servers) > 0 and self.servers[0][2] != self.id:
+        sock = socket.socket()
+        sock.connect((self.servers[0][0], self.servers[0][1]))
+
+        while True:
+          data = sock.recv(1024)
+          if not data:
+            print('server disconnected')
+            break
+          received = pickle.loads(data)
+          print(received, '({} bytes)'.format(len(data)))
+          if received.header == 'greeting':
+            sock.send(Message('registerServer', (self.host, self.port, self.id)).encode())
+          elif received.header == 'updateServers':
+            self.servers = received.body
+            print(self.servers)
+        self.servers.pop(0)
+
+    # start master work
     self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.sock.bind((self.host, self.port))
+    print('listening on {}:{}'.format(self.host, self.port))
     self.sock.listen(1)
 
     while True:
@@ -93,9 +133,10 @@ class Server:
       print(self.observers)
 
 def main(argv):
-  server = Server(argv[0], int(argv[1]))
+  master = (None, None)
   if (len(argv) == 3):
-    server.master = bool(argv[2])
+    master = (argv[1], int(argv[2]))
+  server = Server(int(argv[0]), master[0], master[1])
   server.run()
 
 if __name__ == '__main__':
